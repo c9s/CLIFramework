1396cb3e82baba534c0e6280275ee3492509362f
c9s <cornelius.howl@gmail.com>
2012-01-28 19:29:56 +0800

    Fix boostrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,6 +1,6 @@
 <?php
 require 'tests/helpers.php';
-require 'Universal/ClassLoader/SplClassLoader.php';
+require 'vendor/pear/Universal/ClassLoader/SplClassLoader.php';
 $classLoader = new \Universal\ClassLoader\SplClassLoader(array( 
     'CLIFramework' => 'src',
     'TestApp' => 'tests',
