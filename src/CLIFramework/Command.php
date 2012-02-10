9a9ad2082a6384b46cf8b89d7089a706caa3e5f7
c9s <cornelius.howl@gmail.com>
2012-02-10 22:40:27 +0800

    Build package.xml at 2012-02-10

--- src/CLIFramework/Command.php
+++ src/CLIFramework/Command.php
@@ -1,12 +1,12 @@
 <?php
-/*
+
+/**
  * This file is part of the CLIFramework package.
  *
  * (c) Yo-An Lin <cornelius.howl@gmail.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
- *
  */
 namespace CLIFramework;
 use GetOptionKit\GetOptionKit;
@@ -40,6 +40,5 @@ abstract class Command extends CommandBase
         $app = $this->application;
         return $app::getLogger();
     }
-
-
 }
+
