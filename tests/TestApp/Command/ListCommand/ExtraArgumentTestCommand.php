56563ffc7a954481129d423fe90dcab610dbdff5
c9s <cornelius.howl@gmail.com>
2012-04-25 00:49:13 +0800

    check required option and arguments

--- tests/TestApp/Command/ListCommand/ExtraArgumentTestCommand.php
+++ tests/TestApp/Command/ListCommand/ExtraArgumentTestCommand.php
@@ -1,8 +1,26 @@
 <?php
 namespace TestApp\Command\ListCommand;
+use Exception;
 
 class ExtraArgumentTestCommand extends \CLIFramework\Command
 {
+
+    function options($opts)
+    {
+        $opts->add('as:','required value');
+    }
+
+
+
+    function execute()
+    {
+        if( null === $this->options->as )
+            throw new Exception( '--as option is required.' );
+
+        $args = func_get_args();
+        if( empty($args) )
+            throw new Exception( 'command argument is required' );
+    }
 
 
 
