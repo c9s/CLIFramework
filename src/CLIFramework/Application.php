d75d4e469f94235df4ce326dfba69c87cb0efa3d
c9s <cornelius.howl@gmail.com>
2012-06-30 13:09:38 +0800

    Extract try block to runWithTry

--- src/CLIFramework/Application.php
+++ src/CLIFramework/Application.php
@@ -81,8 +81,18 @@ class Application extends CommandBase
     }
 
 
+    public function runWithTry($argv) 
+    {
+        try {
+            return $this->run($argv);
+        } catch( Exception $e ) {
+            $this->getLogger()->error( $e->getMessage() );
+        }
+        return false;
+    }
+
     /**
-     * run application with 
+     * Run application with 
      * list argv 
      *
      * @param Array $argv
@@ -90,99 +100,91 @@ class Application extends CommandBase
      * */
     public function run(Array $argv)
     {
-        try {
-            $current_cmd = $this;
+        $current_cmd = $this;
 
-            // init application,
-            // before parsing options, we have to known the registered commands.
-            $current_cmd->init();
+        // init application,
+        // before parsing options, we have to known the registered commands.
+        $current_cmd->init();
 
-            // use getoption kit to parse application options
-            $getopt = $this->getoptParser;
-            $specs = new OptionSpecCollection;
-            $getopt->setSpecs( $specs );
+        // use getoption kit to parse application options
+        $getopt = $this->getoptParser;
+        $specs = new OptionSpecCollection;
+        $getopt->setSpecs( $specs );
 
-            // init application options
-            $current_cmd->options($specs);
+        // init application options
+        $current_cmd->options($specs);
 
-            // save options specs
-            $current_cmd->optionSpecs = $specs;
+        // save options specs
+        $current_cmd->optionSpecs = $specs;
 
-            // parse the first part options (options after script name)
-            // option parser should stop before next command name.
-            //
-            //    $ app.php -v -d next
-            //                  |
-            //                  |->> parser
-            //
-            $current_cmd->setOptions( $getopt->parse( $argv ) );
-            $current_cmd->prepare();
+        // parse the first part options (options after script name)
+        // option parser should stop before next command name.
+        //
+        //    $ app.php -v -d next
+        //                  |
+        //                  |->> parser
+        //
+        $current_cmd->setOptions( $getopt->parse( $argv ) );
+        $current_cmd->prepare();
 
-            $command_stack = array();
-            $arguments = array();
+        $command_stack = array();
+        $arguments = array();
 
-            // get command list from application self
-            $subcommand_list = $current_cmd->getCommandList();
-            while( ! $getopt->isEnd() ) 
-            {
-                $a = $getopt->getCurrentArgument();
-
-                // if current command is in subcommand list.
-                if( in_array(  $getopt->getCurrentArgument() , $subcommand_list ) ) 
-                {
-                    $subcommand = $getopt->getCurrentArgument();
-                    $getopt->advance(); // advance position
-
-                    // get command object
-                    $current_cmd = $current_cmd->getCommand( $subcommand );
-
-                    $getopt->setSpecs($current_cmd->optionSpecs);
-
-                    // parse options for command.
-                    $current_cmd_options = $getopt->continueParse();
-
-                    // run subcommand prepare
-                    $current_cmd->setOptions( $current_cmd_options );
-
-                    // echo get_class($current_cmd) , ' => ' , print_r($current_cmd_options);
-
-                    $command_stack[] = $current_cmd; // save command object into the stack
-
-                    // update subcommand list
-                    $subcommand_list = $current_cmd->getCommandList();
-
-                } else {
-                    $a = $getopt->advance();
-                    $arguments[] = $a;
-                }
-            }
-
-            foreach($command_stack as $cmd) {
-                $cmd->prepare();
-            }
-
-
-            // get last command and run
-            if( $last_cmd = array_pop( $command_stack ) ) {
-                $return = $last_cmd->executeWrapper( $arguments );
-                $last_cmd->finish();
-                while( $cmd = array_pop( $command_stack ) ) {
-                    // call finish stage.. of every command.
-                    $cmd->finish();
-                }
-            }
-            else {
-                // no command specified.
-                return $this->executeWrapper( $arguments );
-            }
-
-            $current_cmd->finish();
-        } 
-        catch( Exception $e ) 
+        // get command list from application self
+        $subcommand_list = $current_cmd->getCommandList();
+        while( ! $getopt->isEnd() ) 
         {
-            $this->getLogger()->error( $e->getMessage() );
-            return false;
+            $a = $getopt->getCurrentArgument();
+
+            // if current command is in subcommand list.
+            if( in_array(  $getopt->getCurrentArgument() , $subcommand_list ) ) 
+            {
+                $subcommand = $getopt->getCurrentArgument();
+                $getopt->advance(); // advance position
+
+                // get command object
+                $current_cmd = $current_cmd->getCommand( $subcommand );
+
+                $getopt->setSpecs($current_cmd->optionSpecs);
+
+                // parse options for command.
+                $current_cmd_options = $getopt->continueParse();
+
+                // run subcommand prepare
+                $current_cmd->setOptions( $current_cmd_options );
+
+                // echo get_class($current_cmd) , ' => ' , print_r($current_cmd_options);
+
+                $command_stack[] = $current_cmd; // save command object into the stack
+
+                // update subcommand list
+                $subcommand_list = $current_cmd->getCommandList();
+
+            } else {
+                $a = $getopt->advance();
+                $arguments[] = $a;
+            }
         }
+
+        foreach($command_stack as $cmd) {
+            $cmd->prepare();
+        }
+
+
+        // get last command and run
+        if( $last_cmd = array_pop( $command_stack ) ) {
+            $return = $last_cmd->executeWrapper( $arguments );
+            $last_cmd->finish();
+            while( $cmd = array_pop( $command_stack ) ) {
+                // call finish stage.. of every command.
+                $cmd->finish();
+            }
+        }
+        else {
+            // no command specified.
+            return $this->executeWrapper( $arguments );
+        }
+        $current_cmd->finish();
         return true;
     }
 
