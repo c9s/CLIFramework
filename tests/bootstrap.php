6f83ca10669c4a6aef00d695d16829711e8df8dc
c9s <cornelius.howl@gmail.com>
2013-03-21 16:15:08 +0800

    clean up

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,8 +1,3 @@
 <?php
 require 'PHPUnit/TestMore.php';
 require 'vendor/autoload.php';
-$classLoader = new \Universal\ClassLoader\BasePathClassLoader(array( 
-    'src', 'vendor/pear', 'tests',
-));
-$classLoader->useIncludePath(true);
-$classLoader->register();
