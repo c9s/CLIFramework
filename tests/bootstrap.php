47be846d2ad6248909d0642fe94ddb161f51765e
c9s <cornelius.howl@gmail.com>
2013-03-21 10:41:46 +0800

    udpate composer dependency

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,5 +1,6 @@
 <?php
 require 'tests/helpers.php';
+require 'vendor/autoload.php';
 require 'vendor/pear/Universal/ClassLoader/BasePathClassLoader.php';
 $classLoader = new \Universal\ClassLoader\BasePathClassLoader(array( 
     'src', 'vendor/pear', 'tests',
