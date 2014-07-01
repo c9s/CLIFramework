57081b36bbc106ce8b358e2681b86a010881d8a0
c9s <yoanlin93@gmail.com>
2014-07-01 23:55:46 +0800

    error message for InvalidCommandArgumentException

--- src/CLIFramework/Exception/InvalidCommandArgumentException.php
+++ src/CLIFramework/Exception/InvalidCommandArgumentException.php
@@ -6,12 +6,15 @@ class InvalidCommandArgumentException extends Exception
 {
     public $command;
 
+    public $arg;
+
     public $argIndex;
 
-    public function __construct($command, $argIndex) {
+    public function __construct($command, $argIndex, $arg) {
         $this->command = $command;
         $this->argIndex = $argIndex;
-        parent::__construct();
+        $this->arg = $arg;
+        parent::__construct("Invalid '{$command->getName()}' command argument '$arg' at position $argIndex");
     }
 }
 
