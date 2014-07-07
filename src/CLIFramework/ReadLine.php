4ddd63c71a6e6d6e70e115725935fed392ca1fd8
c9s <yoanlin93@gmail.com>
2014-07-07 21:18:33 +0800

    implemente complete method

--- src/CLIFramework/ReadLine.php
+++ src/CLIFramework/ReadLine.php
@@ -10,6 +10,31 @@ class ReadLine
         $this->autoCompleters[] = $autoCompleter;
     }
 
+
+    public function complete($token, $index) {
+        $info = readline_info();
+        $fullInput = substr($info['line_buffer'], 0, $info['end']);
+
+        $matches = array();
+        foreach( $this->autoCompleters as $completer ) {
+            if ($completer->canComplete($token, $index)) {
+                $_matches = $completer->complete($token, $index);
+                foreacH ($_matches as $_m) {
+                    $matches[] = $m;
+                }
+            }
+        }
+        return $matches;
+    }
+
+
+
+    public function readline($prompt = '> ') {
+        $success = readline_completion_function( array($this,"complete") );
+        $ret = readline($prompt);
+        return $ret;
+    }
+
 }
 
 
