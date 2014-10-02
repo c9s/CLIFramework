2e10b4f13df5477492a57e35566177dc6dfb2903
c9s <yoanlin93@gmail.com>
2014-10-02 23:12:25 +0800

    Add command group description

--- src/CLIFramework/CommandGroup.php
+++ src/CLIFramework/CommandGroup.php
@@ -6,6 +6,8 @@ class CommandGroup
 {
     public $name;
 
+    public $desc;
+
     public $commandNames = array();
 
     public function __construct($groupName, $commands = array())
@@ -16,10 +18,31 @@ class CommandGroup
 
     public function addCommand($commandName) {
         $this->commands[] = $commandName;
+        return $this;
     }
 
     public function getCommands() {
         return $this->commands;
+    }
+
+
+    /**
+     * Set group description
+     *
+     * @param string $desc
+     * @return CommandGroup
+     */
+    public function setDesc($desc)
+    {
+        $this->desc = $desc;
+        return $this;
+    }
+
+    /**
+     * Get the group description
+     */
+    public function getDesc() {
+        return $this->desc;
     }
 }
 
