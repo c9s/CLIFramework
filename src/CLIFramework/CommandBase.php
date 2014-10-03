85547f4b3a3109238c21e0182a362d7da7cb9a58
c9s <yoanlin93@gmail.com>
2014-10-03 23:54:51 +0800

    Refactor correction to Corrector class

--- src/CLIFramework/CommandBase.php
+++ src/CLIFramework/CommandBase.php
@@ -24,6 +24,7 @@ use CLIFramework\Exception\InvalidCommandArgumentException;
 use CLIFramework\Exception\CommandArgumentNotEnoughException;
 use CLIFramework\ArgInfo;
 use CLIFramework\ArgInfoList;
+use CLIFramework\Corrector;
 
 /**
  * Command based class (application & subcommands inherit from this class)
@@ -404,40 +405,8 @@ abstract class CommandBase
     public function guessCommand($commandName) {
         // array of words to check against
         $words = array_keys($this->commands);
-
-        // no shortest distance found, yet
-        $shortest = -1;
-
-        // loop through words to find the closest
-        foreach ($words as $word) {
-
-            // calculate the distance between the input word,
-            // and the current word
-            $lev = levenshtein($commandName, $word);
-
-            // check for an exact match
-            if ($lev == 0) {
-
-                // closest word is this one (exact match)
-                $closest = $word;
-                $shortest = 0;
-
-                // break out of the loop; we've found an exact match
-                break;
-            }
-
-            // if this distance is less than the next found shortest
-            // distance, OR if a next shortest word has not yet been found
-            if ($lev <= $shortest || $shortest < 0) {
-                // set the closest match, and shortest distance
-                $closest  = $word;
-                $shortest = $lev;
-            }
-        }
-        if ($shortest == 0) {
-            return array(0, $closest);
-        }
-        return array($shortest, $closest);
+        $correction = new Corrector($words);
+        return $correction->correct($commandName);
     }
 
 
