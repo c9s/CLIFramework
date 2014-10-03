11a0eb9f948a2e055da6c5a712f3fc879a2c51bc
c9s <yoanlin93@gmail.com>
2014-10-03 23:41:10 +0800

    Improve topic generator

--- src/CLIFramework/Command/BuildGitHubWikiTopicsCommand.php
+++ src/CLIFramework/Command/BuildGitHubWikiTopicsCommand.php
@@ -1,13 +1,13 @@
 <?php
 namespace CLIFramework\Command;
 use CLIFramework\Command;
+use ClassTemplate\Block;
 use ClassTemplate\ClassTemplate;
 use RecursiveDirectoryIterator;
 use RecursiveIteratorIterator;
 use RegexIterator;
 use RecursiveRegexIterator;
 use Exception;
-
 
 class BuildGitHubWikiTopicsCommand extends Command
 {
@@ -19,6 +19,7 @@ class BuildGitHubWikiTopicsCommand extends Command
     {
         $opts->add('ns:', 'Class namespace');
         $opts->add('dir:', 'Output directory');
+        $opts->add('update', 'Update wiki repository');
     }
 
     public function arguments($args) 
@@ -29,10 +30,10 @@ class BuildGitHubWikiTopicsCommand extends Command
 
     public function execute($user, $repo) {
         $ns = '';
-        if ($appNs = $this->getApplication()->getCurrentAppNamespace()) {
-            $ns = $appNs . '\\Topic\\';
-        } elseif ($appNs = $this->options->ns) {
+        if ($appNs = $this->options->ns) {
             $ns = rtrim(str_replace(':','\\',$appNs),'\\') . '\\';
+        } elseif ($appNs = $this->getApplication()->getCurrentAppNamespace()) {
+            $ns = $appNs . '\\Topic\\';
         } else {
             $this->logger->notice('Namespace is defined.');
         }
@@ -45,10 +46,12 @@ class BuildGitHubWikiTopicsCommand extends Command
         $currentDir = getcwd();
 
         if (is_dir($localRepoPath)) {
-            $this->logger->info("Fetching $wikiGitURI...");
-            system("git -C $localRepoPath pull origin", $retval);
-            if ($retval != 0) {
-                return $this->logger->error("Can't clone wiki repository");
+            if ($this->options->update) {
+                $this->logger->info("Fetching $wikiGitURI...");
+                system("git -C $localRepoPath pull origin", $retval);
+                if ($retval != 0) {
+                    return $this->logger->error("Can't clone wiki repository");
+                }
             }
         } else {
             $this->logger->info("Cloning $wikiGitURI...");
@@ -65,6 +68,8 @@ class BuildGitHubWikiTopicsCommand extends Command
 
         $directory = new RecursiveDirectoryIterator($localRepoPath);
         $iterator = new RecursiveIteratorIterator($directory);
+
+        $topics = array();
         foreach($iterator as $file) {
             if (preg_match('/\.git/',$file->getPathName())) {
                 continue;
@@ -73,14 +78,18 @@ class BuildGitHubWikiTopicsCommand extends Command
                 $topicRemoteUrl = $wikiBaseUrl . '/' . $file->getFileName();
 
                 // Used from command-line, to invoke the topic
-                $topicId = strtolower(preg_replace(array('/.md$/'),array(''),$file->getFileName()));
+                // $topicId = strtolower(preg_replace(array('/.md$/'),array(''),$file->getFileName()));
 
                 // The readable topic title
                 // TODO: Support non-ascii characters 
-                $entryName = preg_replace(array('/.md$/','/[^a-zA-Z0-9-]/'),array('',''),$file->getFileName());
-                $topicClassName = join("", array_map("ucfirst", explode('-', $entryName))) . 'Topic';
+                $entryName = preg_replace('/.md$/','',$file->getFileName());
+                $entryNameNonEnChars = trim(preg_replace('/[^a-zA-Z0-9-\s]/', '', $entryName));
+                $topicClassName = join("", array_map("ucfirst", explode('-', str_replace(' ','', $entryNameNonEnChars)))) . 'Topic';
+                $topicId = strtolower(preg_replace('/\s+/','-', $entryNameNonEnChars));
                 $topicTitle = preg_replace('/-/', ' ', $entryName);
                 $topicFullClassName = $ns . $topicClassName;
+
+                $topics[ $topicId ] = $topicFullClassName;
 
                 $classFile = $outputDir . DIRECTORY_SEPARATOR . str_replace('\\', DIRECTORY_SEPARATOR, $topicFullClassName) . '.php';
 
@@ -109,7 +118,16 @@ class BuildGitHubWikiTopicsCommand extends Command
                     throw new Exception("Can't write file $classFile.");
                 }
             }
+
         }
+
+        $this->logger->info(wordwrap("You may now copy the below content to the 'init' method in your application class:"));
+
+        $this->logger->writeln('-------');
+        $block = new Block();
+        $block->appendLine('$this->topics(' . var_export($topics, true) . ');');
+        $this->logger->write($block->render());
+        $this->logger->writeln('-------');
 
         chdir($currentDir);
         $this->logger->info("Done");
