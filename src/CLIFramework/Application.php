195f83210deca103ea11c221b8f7d604bdacc9dc
c9s <yoanlin93@gmail.com>
2014-10-04 20:07:33 +0800

    Checking in changes prior to tagging of version 2.0.0.

--- src/CLIFramework/Application.php
+++ src/CLIFramework/Application.php
@@ -29,7 +29,7 @@ class Application extends CommandBase
     implements CommandInterface
 {
     const CORE_VERSION = '1.10.0';
-    const VERSION = '1.10.0';
+    const VERSION = "2.0.0";
     const NAME = 'CLIFramework';
 
     // options parser
