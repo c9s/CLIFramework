80ebd5f16243c4fee129cead37954c6fb106a112
c9s <yoanlin93@gmail.com>
2014-12-31 10:49:26 +0800

    Add hasSupport static method

--- src/CLIFramework/ConsoleInfo/TputConsoleInfo.php
+++ src/CLIFramework/ConsoleInfo/TputConsoleInfo.php
@@ -4,12 +4,26 @@ namespace CLIFramework\ConsoleInfo;
 class TputConsoleInfo
 {
 
-    public function getColumns() { 
+    public function getColumns() 
+    {
         return intval(exec('tput cols'));
     }
 
-    public function getRows() {
+    public function getRows() 
+    {
         return intval(exec('tput lines'));
+    }
+
+    static public function hasSupport()
+    {
+        $paths = explode(':',getenv('PATH'));
+        foreach($paths as $path) {
+            $bin = $path . DIRECTORY_SEPARATOR . 'tput';
+            if (file_exists($bin) && is_executable($bin)) {
+                return true;
+            }
+        }
+        return false;
     }
 
 }
