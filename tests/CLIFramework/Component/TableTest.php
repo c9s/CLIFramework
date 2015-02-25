b3ad766268d85a54f8b5f9c09a69b204ebb63505
c9s <yoanlin93@gmail.com>
2015-02-25 15:17:02 +0800

    Add test for table without footer

--- tests/CLIFramework/Component/TableTest.php
+++ tests/CLIFramework/Component/TableTest.php
@@ -5,11 +5,43 @@ use CLIFramework\Component\MarkdownTableStyle;
 
 class TableTest extends PHPUnit_Framework_TestCase
 {
-    public function testDefaultTable()
+
+    public function testDefaultTableWithoutFooter()
     {
         $table = new Table;
-        // $table->setStyle(new DefaultTableStyle);
-        $table->setStyle(new MarkdownTableStyle);
+        $table->setStyle(new DefaultTableStyle);
+        $table->setHeaders(array(
+            'Published Date',
+            'Title',
+            'Description',
+        ));
+        $table->addRow(array( 
+            "September 16, 2014",
+            "Zero to One: Notes on Startups, or How to Build the Future",
+            "If you want to build a better future, you must believe in secrets.
+            The great secret of our time is that there are still uncharted frontiers to explore and new inventions to create. In Zero to One, legendary entrepreneur and investor Peter Thiel shows how we can find singular ways to create those new things. ",
+        ));
+        $table->addRow(array( 
+            "November 4, 2014",
+            "Hooked: How to Build Habit-Forming Products",
+
+            "Why do some products capture widespread attention while others flop? What makes us engage with certain products out of sheer habit? Is there a pattern underlying how technologies hook us? "
+            . "Nir Eyal answers these questions (and many more) by explaining the Hook Model—a four-step process embedded into the products of many successful companies to subtly encourage customer behavior. Through consecutive “hook cycles,” these products reach their ultimate goal of bringing users back again and again without depending on costly advertising or aggressive messaging.\n"
+        ));
+        $out = $table->render();
+
+        if (!file_exists('tests/data/default-table.txt')) {
+            file_put_contents('tests/data/default-table.txt', $out);
+            echo "\n" . $out . "\n";
+        }
+        $this->assertStringEqualsFile('tests/data/default-table.txt', $out);
+    }
+
+
+    public function testDefaultTableWithFooter()
+    {
+        $table = new Table;
+        $table->setStyle(new DefaultTableStyle);
         $table->setHeaders(array(
             'Published Date',
             'Title',
@@ -31,11 +63,11 @@ class TableTest extends PHPUnit_Framework_TestCase
         $table->setFooter('Found 3 books...');
         $out = $table->render();
 
-        if (!file_exists('tests/data/default-table.txt')) {
-            file_put_contents('tests/data/default-table.txt', $out);
+        if (!file_exists('tests/data/default-table-footer.txt')) {
+            file_put_contents('tests/data/default-table-footer.txt', $out);
             echo "\n" . $out . "\n";
         }
-        $this->assertStringEqualsFile('tests/data/default-table.txt', $out);
+        $this->assertStringEqualsFile('tests/data/default-table-footer.txt', $out);
     }
 
 
