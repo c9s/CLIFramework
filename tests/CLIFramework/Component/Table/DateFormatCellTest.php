a0fb998d81b61879b96ea04781884f6d2eb5d070
c9s <yoanlin93@gmail.com>
2015-02-25 20:58:24 +0800

    use assertStringMatchesFormat for testing

--- tests/CLIFramework/Component/Table/DateFormatCellTest.php
+++ tests/CLIFramework/Component/Table/DateFormatCellTest.php
@@ -7,7 +7,7 @@ class DateFormatCellTest extends PHPUnit_Framework_TestCase
     {
         $dateFormatCell = new DateFormatCell('en_US');
         $str = $dateFormatCell->format(0);
-        $this->assertEquals('Thursday, January 1, 1970 at 8:00:00 AM Taipei Standard Time', $str);
+        $this->assertStringMatchesFormat('Thursday, January %d, %d at 8:00:00 AM %s', $str);
     }
 }
 
