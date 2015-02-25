66e296ec5b11572ad61cc48ebff4ceec7d8a21be
c9s <yoanlin93@gmail.com>
2015-02-25 15:02:46 +0800

    Add TableStyle

---
+++ src/CLIFramework/Component/MarkdownTableStyle.php
@@ -0,0 +1,17 @@
+<?php
+namespace CLIFramework\Component;
+
+class MarkdownTableStyle {
+    public $cellPadding = 1;
+    public $cellPaddingChar = ' ';
+    public $verticalBorderChar = '|';
+
+    public $rowSeparatorCrossChar = '|';
+    public $rowSeparatorBorderChar = '-';
+
+    public $rowSeparatorLeftmostCrossChar = '|';
+    public $rowSeparatorRightmostCrossChar = '|';
+
+    public $drawTableBorder = false;
+}
+
