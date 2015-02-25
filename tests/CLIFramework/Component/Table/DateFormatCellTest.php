c820b7bbadd1d19acf4868e950806570557620c0
c9s <yoanlin93@gmail.com>
2015-02-25 21:00:35 +0800

    set timezone to pass tests on different machine

--- tests/CLIFramework/Component/Table/DateFormatCellTest.php
+++ tests/CLIFramework/Component/Table/DateFormatCellTest.php
@@ -5,9 +5,9 @@ class DateFormatCellTest extends PHPUnit_Framework_TestCase
 {
     public function testDateFormat()
     {
-        $dateFormatCell = new DateFormatCell('en_US');
+        $dateFormatCell = new DateFormatCell('en_US', IntlDateFormatter::FULL, IntlDateFormatter::FULL, 'America/Los_Angeles');
         $str = $dateFormatCell->format(0);
-        $this->assertStringMatchesFormat('Thursday, January %d, %d at 8:00:00 AM %s', $str);
+        $this->assertStringMatchesFormat('Wednesday, December %d, %d at 4:00:00 PM Pacific Standard Time', $str);
     }
 }
 
