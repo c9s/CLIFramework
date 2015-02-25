0a1cf334bc721ffdfa708c88a5f659289c26cbdc
c9s <yoanlin93@gmail.com>
2015-02-25 20:56:15 +0800

    Support formatObject method

--- src/CLIFramework/Component/Table/DateFormatCell.php
+++ src/CLIFramework/Component/Table/DateFormatCell.php
@@ -3,9 +3,21 @@ namespace CLIFramework\Component\Table;
 use CLIFramework\Component\Table\CellAttribute;
 use NumberFormatter;
 use IntlDateFormatter;
+use DateTime;
 
 class DateFormatCell extends NumberFormatCell
 {
+
+    /**
+     * IntlDateFormatter::FULL (integer)
+        Completely specified style (Tuesday, April 12, 1952 AD or 3:30:42pm PST)
+     * IntlDateFormatter::LONG (integer)
+        Long style (January 12, 1952 or 3:30:32pm)
+     * IntlDateFormatter::MEDIUM (integer)
+        Medium style (Jan 12, 1952)
+     * IntlDateFormatter::SHORT (integer)
+        Most abbreviated style, only essential data (12/13/52 or 3:30pm)
+     */
     public function __construct($locale, $datetype = IntlDateFormatter::FULL, $timetype = IntlDateFormatter::FULL, $timezone = NULL, $calendar = IntlDateFormatter::GREGORIAN, $pattern = "")
     {
         $this->locale = $locale;
@@ -13,6 +25,9 @@ class DateFormatCell extends NumberFormatCell
     }
 
     public function format($cell) {
+        if ($cell instanceof DateTime) {
+            return $this->formatter->formatObject($cell);
+        }
         return $this->formatter->format($cell);
     }
 }
