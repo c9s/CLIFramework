decb60f36466e9fd7f14404a7d69dc6c5575bfe0
c9s <yoanlin93@gmail.com>
2015-02-25 18:47:05 +0800

    Add DurationFormatCell

---
+++ src/CLIFramework/Component/Table/DurationFormatCell.php
@@ -0,0 +1,18 @@
+<?php
+namespace CLIFramework\Component\Table;
+use CLIFramework\Component\Table\CellAttribute;
+use NumberFormatter;
+
+class DurationFormatCell extends NumberFormatCell
+{
+    protected $locale;
+
+    protected $formatter;
+
+    public function __construct($locale) {
+        $this->locale = $locale;
+        $this->formatter = new NumberFormatter($locale, NumberFormatter::DURATION); 
+    }
+}
+
+
