45c12e58a0f877b5e4a730608aeb05b2659b2845
c9s <yoanlin93@gmail.com>
2015-04-18 13:14:02 +0800

    use assertion methods to test

--- tests/CLIFramework/TestAppCommandTest.php
+++ tests/CLIFramework/TestAppCommandTest.php
@@ -6,25 +6,22 @@ class TestAppCommandTest extends PHPUnit_Framework_TestCase
     public function testSimpleCommand()
     {
         $command = new TestApp\Command\SimpleCommand(new TestApp\Application);
-        ok($command);
 
         $argInfos = $command->getArgumentsInfo();
-        ok($argInfos);
-
-        count_ok(1, $argInfos);
-        is('var', $argInfos[0]->name);
+        $this->assertNotEmpty($argInfos);
+        $this->assertCount(1, $argInfos);
+        $this->assertEquals('var', $argInfos[0]->name);
     }
 
     public function testArginfoCommand() {
         $cmd = new TestApp\Command\ArginfoCommand(new TestApp\Application);
-        ok($cmd);
 
         $argInfos = $cmd->getArgumentsInfo();
-        ok($argInfos);
-        count_ok(3, $argInfos);
+        $this->assertNotEmpty($argInfos);
+        $this->assertCount(3, $argInfos);
 
         foreach( $argInfos as $arginfo ) {
-            ok( $arginfo instanceof ArgInfo);
+            $this->assertInstanceOf('CLIFramework\ArgInfo',  $arginfo);
         }
     }
 }
