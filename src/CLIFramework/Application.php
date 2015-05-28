21eb2cad73abf72f327b40855f9cf24b27c22b15
c9s <yoanlin93@gmail.com>
2015-05-28 13:31:32 +0800

    Checking in changes prior to tagging of version 2.5.4.

--- src/CLIFramework/Application.php
+++ src/CLIFramework/Application.php
@@ -37,7 +37,7 @@ class Application extends CommandBase
     implements CommandInterface
 {
     const CORE_VERSION = '1.10.0';
-    const VERSION = "2.5.3";
+    const VERSION = "2.5.4";
     const NAME = 'CLIFramework';
 
 
