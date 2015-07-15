ec1f2abf5e7123d06947dbfdd6b835c0d430ea35
c9s <yoanlin93@gmail.com>
2015-07-15 09:54:58 +0800

    use codegen to generate autoload script

--- src/CLIFramework/Command/BuildPharCommand.php
+++ src/CLIFramework/Command/BuildPharCommand.php
@@ -6,6 +6,11 @@ use RecursiveIteratorIterator;
 use RecursiveDirectoryIterator;
 use RuntimeException;
 use Phar;
+use CodeGen\Expr\NewObjectExpr;
+use CodeGen\Block;
+use CodeGen\Statement\UseStatement;
+use CodeGen\Statement\AssignStatement;
+use CodeGen\Statement\MethodCallStatement;
 
 /**
  * Build phar file from composer.json
@@ -27,11 +32,11 @@ class BuildPharCommand extends Command
         $args->add('composer-config')->isa('file');
     }
 
-    public function traceAutoloadsWithComposerJson($composerJson = 'composer.json', $includeRootDev = false)
+    public function traceAutoloadsWithComposerJson($composerJson = 'composer.json', $vendorDir = 'vendor', $includeRootDev = false)
     {
         $json = file_get_contents($composerJson);
         $obj = json_decode($json, true);
-        return $this->traceAutoloads($obj, $includeRootDev);
+        return $this->traceAutoloads($obj, $vendorDir, $includeRootDev);
     }
 
     public function traceAutoloadsWithRequirements(array $requirements = array(), $vendorDir = 'vendor')
@@ -48,16 +53,17 @@ class BuildPharCommand extends Command
             if (!file_exists($packageComposerJson)) {
                 throw new RuntimeException("Missing composer.json file: $packageComposerJson");
             }
-            $packageAutoloads = $this->traceAutoloadsWithComposerJson($packageComposerJson, false); // don't include require-dev for dependencies
+            $packageAutoloads = $this->traceAutoloadsWithComposerJson($packageComposerJson, $vendorDir, false); // don't include require-dev for dependencies
             $autoloads = array_merge($autoloads, $packageAutoloads);
         }
         return $autoloads;
     }
 
-    public function traceAutoloads(array $config, $includeRootDev = false, $vendorDir = 'vendor')
+    public function traceAutoloads(array $config, $vendorDir = 'vendor', $includeRootDev = false )
     {
         $autoloads = array();
 
+        // @see https://getcomposer.org/doc/02-libraries.md#platform-packages
         if (isset($config['require'])) {
             $autoloads = array_merge($autoloads, $this->traceAutoloadsWithRequirements($config['require']));
         }
@@ -66,10 +72,34 @@ class BuildPharCommand extends Command
         }
 
         if (isset($config['autoload'])) {
+            // Rewrite autoload path with vendor base dir
+            // $config['autoload'] = $this->prependAutoloadPathPrefix($config['autoload'], $vendorDir . DIRECTORY_SEPARATOR . $config['name'] . DIRECTORY_SEPARATOR);
             $autoloads[ $config['name'] ] = $config['autoload'];
         }
         return $autoloads;
     }
+
+    public function prependAutoloadPathPrefix($autoloads, $prefix)
+    {
+        $newAutoloads = array();
+        foreach ($autoloads as $autoloadType => & $autoloadConfig) {
+            $newConfig = array();
+            foreach ($autoloadConfig as $ns => $path) {
+                if (is_array($path)) {
+                    $newConfig[ $ns ] = array_map($path, function($p) use ($prefix) {
+                        return $prefix . $p;
+                    });
+
+                } else {
+                    $newConfig[ $ns ] = $prefix . $path;
+                }
+            }
+            $newAutoloads[ $autoloadType ] = $newConfig;
+        }
+        return $newAutoloads;
+    }
+
+
 
     public function execute($composerConfigFile)
     {
@@ -78,16 +108,36 @@ class BuildPharCommand extends Command
         }
 
         $autoloads = $this->traceAutoloadsWithComposerJson($composerConfigFile);
+
+        $block   = new Block;
+        $block[] = new UseStatement('Universal\\ClassLoader\\Psr0ClassLoader');
+        $block[] = new UseStatement('Universal\\ClassLoader\\Psr4ClassLoader');
+
+        $psr0 = array();
+        $psr4 = array();
+        $files = array();
         foreach($autoloads as $packageName => $autoload) {
-            var_dump( $autoload );
             if (isset($autoload['psr-4'])) {
-
+                $psr4 = array_merge($psr4, $autoload['psr-4']);
             } else if (isset($autoload['prs-0'])) {
-
+                $psr0 = array_merge($psr0, $autoload['psr-0']);
             } else if (isset($autoload['files'])) {
-
+                $files = array_merge($autoload['files']);
             }
         }
+
+        if (!empty($psr4)) {
+            $block[] = new AssignStatement('$psr4', new NewObjectExpr('Psr4ClassLoader', [$psr4]));
+            $block[] = new MethodCallStatement('$psr4','register',[ false ]);
+        }
+
+        if (!empty($psr0)) {
+            $block[] = new AssignStatement('$psr0', new NewObjectExpr('Psr0ClassLoader', [$psr0]));
+            $block[] = new MethodCallStatement('$psr0','register',[ false ]);
+        }
+
+        
+        echo $block->render([]);
     }
 
 }
