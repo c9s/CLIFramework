e29cf2cd89fdf7d1e97bcd4471568ffdfd6100c0
c9s <yoanlin93@gmail.com>
2015-07-15 12:30:13 +0800

    Add phar debug script

---
+++ scripts/phar-debug.php
@@ -0,0 +1,12 @@
+<?php
+if (isset($argv[1])) {
+    $file = $argv[1];
+} else {
+    $file = 'app.phar';
+}
+$phar = new Phar($file, 0);
+foreach ($phar as $file) {
+    echo $file->getPathname(), PHP_EOL;
+}
+echo "Stub:\n";
+echo $phar->getStub();
