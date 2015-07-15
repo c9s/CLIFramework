db29516b7b5db83ea2a40c73567d680cd0bef036
c9s <yoanlin93@gmail.com>
2015-07-15 11:40:07 +0800

    Add PharGenerator

---
+++ src/CLIFramework/PharKit/PharGenerator.php
@@ -0,0 +1,56 @@
+<?php
+namespace CLIFramework\PharKit;
+use Phar;
+use CLIFramework\Logger;
+
+
+class PharGenerator
+{
+    protected $pharFile;
+
+    protected $alias;
+
+    protected $map;
+
+    protected $phar;
+
+    protected $shellbang = '#!/usr/bin/env php';
+
+    protected $logger;
+
+    public function __construct(Logger $logger, $pharFile, $alias = null)
+    {
+        $this->logger = $logger;
+        $this->pharFile = $pharFile;
+
+        if ($alias) {
+            $this->alias = $alias;
+        } else {
+            $this->alias = basename($alias);
+        }
+
+        $this->phar = new Phar($this->pharFile, 0, $this->alias);
+        $this->phar->setSignatureAlgorithm(Phar::SHA1);
+    }
+
+    public function shellbang($shellbang)
+    {
+        $this->shellbang = $shellbang;
+    }
+
+    public function getPhar()
+    {
+        return $this->phar;
+    }
+
+    public function generate()
+    {
+        $this->phar->startBuffering();
+   
+   
+    }
+
+
+
+}
+
